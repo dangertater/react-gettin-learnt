8b0fa4f80bcbdcbe6bbf8b04a4f3df021f2c7147
dangertater <106702944+dangertater@users.noreply.github.com>
2022-11-09 13:11:23 -0700

    idk man

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
-import React from "react"
+import React, { useState } from "react"
+import FullTodoList from "./FullTodoList"
 
 export default function App() {
-	return null
+	let [TodoList, setTodoList] = useState([
+		{ id: 1, name: "todo", finished: false },
+	])
+	return (
+		<>
+			<FullTodoList TodoList={TodoList} />
+			<input type="text" />
+			<button>add todo</button>
+			<button>clear checked</button>
+		</>
+	)
 }
