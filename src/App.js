b7765a0137b145e6190c28610cc86f506e1195f2
dangertater <106702944+dangertater@users.noreply.github.com>
2022-11-09 11:54:33 -0700

    lost it all, remaking the todolist with wsd

--- src/App.js
+++ src/App.js
@@ -1,18 +1,5 @@
-function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React f
-				</a>
-			</header>
-		</div>
-	)
-}
+import React from "react"
 
-export default App
+export default function App() {
+	return null
+}
